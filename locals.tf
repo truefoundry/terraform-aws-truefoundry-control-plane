7842f03524673c66b92b51cf2770dd5a5d636078
Vedant Pareek <dunefro@gmail.com>
2023-09-22 02:22:32 +0530

    removed mlmonitoring and mlfoundry name from locals.tf

--- locals.tf
+++ locals.tf
@@ -4,9 +4,7 @@ locals {
 
   truefoundry_db_unique_name = var.truefoundry_db_enable_override ? var.truefoundry_db_override_name : "${var.cluster_name}-db"
 
-  # mlfoundry_unique_name    = "${var.cluster_name}-${var.mlfoundry_name}"
   svcfoundry_unique_name = "${var.cluster_name}-${var.svcfoundry_name}"
-  # mlmonitoring_unique_name = "${var.cluster_name}-${var.mlmonitoring_name}"
 
   truefoundry_db_port            = 5432
   truefoundry_db_master_username = "root"
