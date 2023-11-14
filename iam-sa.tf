0f21be35c95130c23c7a8c25deae3b3b99f81f31
Vedant Pareek <dunefro@gmail.com>
2023-11-14 14:39:26 +0530

    Unified truefoundry role

--- iam-sa.tf
+++ iam-sa.tf
@@ -1,36 +1,19 @@
 # From https://github.com/terraform-aws-modules/terraform-aws-eks/blob/master/examples/irsa/irsa.tf
 
-module "mlfoundry_oidc_iam" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "5.27.0"
-
-  create_role  = true
-  role_name    = "${var.cluster_name}-mlfoundry-deps"
-  provider_url = replace(var.cluster_oidc_issuer_url, "https://", "")
-  oidc_fully_qualified_subjects = [
-    "system:serviceaccount:${var.mlfoundry_k8s_namespace}:${var.mlfoundry_k8s_service_account}"
-  ]
-
-  role_policy_arns = [
-    aws_iam_policy.truefoundry_bucket_policy.arn,
-    aws_iam_policy.truefoundry_assume_role_all.arn,
-  ]
-  tags = local.tags
-}
-
 data "aws_iam_policy" "servicefoundry_ecr_policy" {
   name = "AmazonEC2ContainerRegistryFullAccess"
 }
 
-module "svcfoundry_oidc_iam" {
+module "truefoundry_oidc_iam" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "5.27.0"
 
   create_role  = true
-  role_name    = "${var.cluster_name}-svcfoundry-deps"
+  role_name    = "${var.cluster_name}-truefoundry-deps"
   provider_url = replace(var.cluster_oidc_issuer_url, "https://", "")
   oidc_fully_qualified_subjects = [
-    "system:serviceaccount:${var.svcfoundry_k8s_namespace}:${var.svcfoundry_k8s_service_account}"
+    "system:serviceaccount:${var.svcfoundry_k8s_namespace}:${var.svcfoundry_k8s_service_account}",
+    "system:serviceaccount:${var.mlfoundry_k8s_namespace}:${var.mlfoundry_k8s_service_account}"
   ]
 
   role_policy_arns = [
