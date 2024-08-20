000fdd04f3d80fcbb93466ab97fdcf2f5c5255e0
Vedant Pareek <36420365+dunefro@users.noreply.github.com>
2024-08-21 01:23:59 +0530

    Added IAM role description

--- iam-sa.tf
+++ iam-sa.tf
@@ -14,6 +14,7 @@ module "truefoundry_oidc_iam" {
     "system:serviceaccount:${var.tfy_workflow_admin_k8s_namespace}:${var.tfy_workflow_admin_k8s_service_account}"
   ]
 
+  role_description = "Truefoundry IAM role for ${var.svcfoundry_name}, ${var.mlfoundry_name} and ${var.tfy_workflow_admin_name} in cluster ${var.cluster_name}"
   role_policy_arns = [
     aws_iam_policy.truefoundry_bucket_policy[0].arn,
     aws_iam_policy.svcfoundry_access_to_ssm[0].arn,
