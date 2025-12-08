d4022eedde6f90933a499a73a04bde8af8b93cbe
Vedant Pareek <dunefro@gmail.com>
2025-12-08 23:47:28 +0530

    Create the DB parameter group only when it is needed

--- rds.tf
+++ rds.tf
@@ -99,7 +99,7 @@ resource "aws_db_instance" "truefoundry_db" {
 }
 
 resource "aws_db_parameter_group" "truefoundry_db_parameter_group" {
-  count  = var.truefoundry_db_postgres_parameter_group_override_enabled ? 0 : 1
+  count  = var.truefoundry_db_postgres_parameter_group_enabled && var.truefoundry_db_postgres_parameter_group_override_enabled ? 0 : 1
   name   = "${local.truefoundry_db_unique_name}-rds-pg"
   family = local.postgres_parameter_group_family
 
